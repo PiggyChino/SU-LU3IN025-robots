62475c668d4c6ab11a3838d17dbcff5267ff61af
gautierc1 <99737904+gautierc1@users.noreply.github.com>
2024-02-04 22:34:09 +0100

    Add files via upload

--- paintwars_team_challenger.py
+++ paintwars_team_challenger.py
@@ -32,7 +32,9 @@ def hateBot(robotId, sensors):
     #   sensors["sensor_front_right"]["distance_to_robot"]
 
 	translation = 1 * sensors["sensor_front"]["distance_to_robot"]
-	rotation = (-1) * sensors["sensor_front_left"]["distance_to_robot"] + (1) * sensors["sensor_front_right"]["distance_to_robot"]
+	rotation = (-1) * sensors["sensor_front_left"]["distance"] + 1 * sensors["sensor_front_right"]["distance"] \
+            + (-1) * sensors["sensor_front"]["distance_to_wall"] + 1 * sensors["sensor_front"]["distance_to_robot"] \
+            + (-1) * sensors["sensor_left"]["distance"] + 1 * sensors["sensor_right"]["distance"]
 
     # limite les valeurs de sortie entre -1 et +1
 	translation = max(-1,min(translation,1))
@@ -44,7 +46,9 @@ def hateBot(robotId, sensors):
 def avoider(robotId, sensors):
 	sensors = get_extended_sensors(sensors)
 	translation = 1 * sensors["sensor_front"]["distance"]
-	rotation = (-1) * sensors["sensor_front_left"]["distance"] + (1) * sensors["sensor_front_right"]["distance"]
+	rotation = (-1) * sensors["sensor_front_left"]["distance"] + 1 * sensors["sensor_front_right"]["distance"] \
+            + (-1) * sensors["sensor_front"]["distance_to_wall"] + 1 * sensors["sensor_front"]["distance_to_robot"] \
+            + (-1) * sensors["sensor_left"]["distance"] + 1 * sensors["sensor_right"]["distance"]
 
     # limite les valeurs de sortie entre -1 et +1
 	translation = max(-1,min(translation,1))
@@ -56,7 +60,9 @@ def avoider(robotId, sensors):
 def hateWall(robotId, sensors):
 	sensors = get_extended_sensors(sensors)
 	translation = 1 * sensors["sensor_front"]["distance_to_wall"]
-	rotation = (-1) * sensors["sensor_front_left"]["distance_to_wall"] + (1) * sensors["sensor_front_right"]["distance_to_wall"]
+	rotation = (-1) * sensors["sensor_front_left"]["distance"] + 1 * sensors["sensor_front_right"]["distance"] \
+            + (-1) * sensors["sensor_front"]["distance_to_wall"] + 1 * sensors["sensor_front"]["distance_to_robot"] \
+            + (-1) * sensors["sensor_left"]["distance"] + 1 * sensors["sensor_right"]["distance"]
 
     # limite les valeurs de sortie entre -1 et +1
 	translation = max(-1,min(translation,1))
@@ -65,15 +71,51 @@ def hateWall(robotId, sensors):
 	return translation, rotation
 	
 	
-def loveBot(robotId, sensors):
-	translation = 1 * sensors["sensor_front"]["distance_to_robot"]
-	rotation = (1) * sensors["sensor_front_left"]["distance_to_robot"] + (-1) * sensors["sensor_front_right"]["distance_to_robot"]
+	
+def suiveur_mur(robotId, sensors):
+	if 0.1 > sensors["sensor_front_left"]["distance_to_wall"] or 0.1 > sensors["sensor_front_right"]["distance_to_wall"]:
+		return 1, 1
+	if 0.1 > sensors["sensor_left"]["distance_to_wall"] or 0.1 > sensors["sensor_right"]["distance_to_wall"]:
+		return 1, 0.25
+	if 1 == sensors["sensor_right"]["distance_to_wall"] and 1 == sensors["sensor_front"]["distance_to_wall"]: #si rien devant et a droite, avance
+		return 0.9 , 0.5
+			
+	if 1 > sensors["sensor_front"]["distance_to_wall"]: #si mur devant, tourne a gauche
+		return 0.05 , -1
 
-    # limite les valeurs de sortie entre -1 et +1
-	translation = max(-1,min(translation,1))
-	rotation = max(-1, min(rotation, 1))
+	return 1, 0
+	
+	
+	
+def perturbateur(robotId, sensors):
+	if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == True: #evite les alliés et les murs
+		if sensors["sensor_front_right"]["distance_to_robot"] and sensors["sensor_front_left"]["distance_to_robot"]:
+			return hateBot(robotId, sensors)
+		if sensors["sensor_front_right"]["distance_to_wall"] and sensors["sensor_front_left"]["distance_to_wall"]:
+			return hateWall(robotId, sensors)
+	if sensors["sensor_front"]["distance_to_wall"]:
+		return hateWall(robotId, sensors)	
+		
+	if sensors["sensor_right"] and sensors["sensor_left"]:
+		return 0, -0.7			
+	return avoider(robotId, sensors)
 
-	return translation, rotation
+
+def aleatoire(robotId, sensors):
+	if uniqint % 30 == 0 :
+		rotation = random.randint(-1, 1) * sensors["sensor_front_left"]["distance_to_wall"] + random.randint(-1, 1) *sensors["sensor_front_right"]["distance_to_wall"]
+		return 1, rotation
+	return avoider(robotId, sensors)
+	
+	
+def force(robotId, sensors):
+	if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == False:
+		return 1, 0
+	if uniqint % 30 < 5:
+		return base(robotId, sensors)
+	return hateWall(robotId, sensors)
+    
+	
 
 
 def base(robotId, sensors):
@@ -92,81 +134,26 @@ def step(robotId, sensors):
 	translation = 1 # vitesse de translation (entre -1 et +1)
 	rotation = 0 # vitesse de rotation (entre -1 et +1)
 	sensors = get_extended_sensors(sensors)
-	"""
-	if sensors["sensor_front_left"]["distance"] < 1 or sensors["sensor_front"]["distance"] < 1:
-		rotation = 0.5  # rotation vers la droite
-	elif sensors["sensor_front_right"]["distance"] < 1:
-		rotation = -0.5  # rotation vers la gauche
-        
-    
-	if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == False:
-		enemy_detected_by_front_sensor = True # exemple de détection d'un robot de l'équipe adversaire (ne sert à rien)
-	"""
+
 	uniqint += 1
 	if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == True and (sensors["sensor_front"]["distance_to_robot"] < 0.2 or sensors["sensor_front_right"]["distance_to_robot"] < 0.2 or sensors["sensor_front_left"]["distance_to_robot"] < 0.2): #evite les alliés et les murs
-		return 0, 1
-	match (robotId%8):
+		return avoider(robotId, sensors)
+	if uniqint % 2000 == 0:
+		robotId += 2
+	match (robotId %8):
 		
 		case 0 | 6 : #subsomption, perturbateur				
-			if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == True: #evite les alliés et les murs
-				if sensors["sensor_front_right"]["distance_to_robot"] and sensors["sensor_front_left"]["distance_to_robot"]:
-					return hateBot(robotId, sensors)
-				if sensors["sensor_front_right"]["distance_to_wall"] and sensors["sensor_front_left"]["distance_to_wall"]:
-					return hateWall(robotId, sensors)
-			if sensors["sensor_front"]["distance_to_wall"]:
-				return hateWall(robotId, sensors)	
-				
-			if sensors["sensor_right"] and sensors["sensor_left"]:
-				return 0, -0.7
-				
-			return base(robotId, sensors)
+			return perturbateur(robotId, sensors)
 		
-			
-			
 		case 1 | 4: #aleatoire périodique
-			if uniqint % 30 == 0 :
-				rotation = random.randint(-1, 1) * sensors["sensor_front_left"]["distance_to_wall"] + random.randint(-1, 1) *sensors["sensor_front_right"]["distance_to_wall"]
-				return 1, rotation
-			return base(robotId, sensors)
+			return aleatoire(robotId, sensors)
+				
+		case 2 | 3 : #force le passage
+			return force(robotId, sensors)
 			
+		case 7 : 
+			return suiveur_mur(robotId, sensors)
 			
-			
-			
-		case 3 : #force le passage
-			if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == False:
-				return avoider(robotId, sensors)
-			elif uniqint % 30 < 5:
-				return base(robotId, sensors)
-			return hateWall(robotId, sensors)
-			
-		
-			
-			
-		case 2:
-			if (0 == sensors["sensor_front"]["distance"]) and (0 == sensors["sensor_left"]["distance"]):
-				return 1, 0.5
-			if (0 == sensors["sensor_front"]["distance"]) and (0 == sensors["sensor_right"]["distance"]):
-				return 1, -0,5
-			if (True == sensors["sensor_back"]["isRobot"]) and (False == sensors["sensor_back"]["isSameTeam"]):
-				return 1, 1
-			return base(robotId, sensors)
-			
-			
-			
-		case 7 : #longeur de mur
-			if 0.1 > sensors["sensor_front_left"]["distance_to_wall"] or 0.1 > sensors["sensor_front_right"]["distance_to_wall"]:
-				return 1, 1
-			if 0.1 > sensors["sensor_left"]["distance_to_wall"] or 0.1 > sensors["sensor_right"]["distance_to_wall"]:
-				return 1, 0.25
-			if 1 == sensors["sensor_right"]["distance_to_wall"] and 1 == sensors["sensor_front"]["distance_to_wall"]: #si rien devant et a droite, avance
-				return 0.9 , 0.5
-			
-			if 1 > sensors["sensor_front"]["distance_to_wall"]: #si mur devant, tourne a gauche
-				return 0.05 , -1
-
-			return 1, 0
-			
-			
-		case 4 | 5:
+		case 5:
 			return base(robotId, sensors)
 
